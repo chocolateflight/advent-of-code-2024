b2fb519157b577f71f93832e0d5f552f70054c58
Marc Hostettler <marc.hostettler2@bluewin.ch>
2024-12-03 17:58:15 +0100

    part 1 complete

--- Day03/03-12-2024.py
+++ Day03/03-12-2024.py
@@ -1,10 +1,26 @@
 from pathlib import Path 
+import re
 
 file_path = Path(__file__).with_name("input.txt")
 with file_path.open("r") as f:
-    file_content = f.readlines()
+    file_content = f.read()
     
 # -----Code Part 1-----
+def find_pattern(input):
+    pattern1 = re.compile(r"mul\([0-9]{1,3},[0-9]{1,3}\)", re.IGNORECASE)
+    pattern2 = re.compile(r"[0-9]{1,3},[0-9]{1,3}", re.IGNORECASE)
+    new_content = pattern1.findall(input)
+    numbers = [pattern2.findall(item)[0].split(",") for item in new_content]
+    numbers = [[int(item[0]), int(item[1])] for item in numbers]
+    return numbers
+  
+combination = find_pattern(file_content)
+
+sum=0
+for item in combination:
+  sum += (item[0]*item[1])
+
+print(sum)
 
 
 
