2b1be876147c1f59bbf0d3a6ea1e2535690ee828
Marc Hostettler <marc.hostettler2@bluewin.ch>
2024-12-03 23:20:21 +0100

    Completed Part 2

--- Day03/03-12-2024.py
+++ Day03/03-12-2024.py
@@ -1,10 +1,12 @@
 from pathlib import Path 
 import re
 
-file_path = Path(__file__).with_name("test.txt")
+file_path = Path(__file__).with_name("input.txt")
 with file_path.open("r") as f:
     file_content = f.read()
-    
+
+# -----General----
+
 def multiply(input):
   sum = 0
   for item in input:
@@ -21,19 +23,41 @@ def process_pattern(input, p2):
   numbers = [pattern2.findall(item)[0].split(",") for item in input]
   numbers = [[int(item[0]), int(item[1])] for item in numbers]
   return numbers
-  
+
+number_pattern = r"[0-9]{1,3},[0-9]{1,3}"
   
 # -----Code Part 1-----
 pattern1_part1 = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
-pattern2_part1 = r"[0-9]{1,3},[0-9]{1,3}"
 
-combination = find_pattern(file_content, pattern1_part1)
-processed_combnination = process_pattern(combination, pattern2_part1)
-part1_result = multiply(processed_combnination)
+combination1 = find_pattern(file_content, pattern1_part1)
+processed_combnination1 = process_pattern(combination1, number_pattern)
+part1_result = multiply(processed_combnination1)
 
 print(part1_result)
 
 # -----Code Part 2-----
+pattern1_part2 = r"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)"
 
+def clean_instructions(input):
+  skip = False
+  instructions = []
+  
+  for item in input:
+    if item == "don't()":
+      skip = True
+    elif item == "do()":
+      skip = False
+    elif not skip:
+      instructions.append(item)
+      
+  return instructions
+  
+
+combination2 = find_pattern(file_content, pattern1_part2)
+clean_combinations = clean_instructions(combination2)
+processed_combnination2 = process_pattern(clean_combinations, number_pattern)
+part2_result = multiply(processed_combnination2)
+
+print(part2_result)
 
 # -----Documentation-----
