8f8d9c35417acec115ff4aa2d5aebd98849165aa
Marc Hostettler <marc.hostettler2@bluewin.ch>
2024-12-03 17:33:15 +0100

    init day 3

---
+++ Day03/03-12-2024.py
@@ -0,0 +1,15 @@
+from pathlib import Path 
+
+file_path = Path(__file__).with_name("input.txt")
+with file_path.open("r") as f:
+    file_content = f.readlines()
+    
+# -----Code Part 1-----
+
+
+
+# -----Code Part 2-----
+
+
+
+# -----Documentation-----
